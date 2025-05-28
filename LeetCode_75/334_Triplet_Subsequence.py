4e5953720733f91fd5e241283633afbdd4286103
zdebar <157598603+zdebar@users.noreply.github.com>
2025-05-28 09:42:43 +0200

    15 /  75 repair

--- LeetCode_75/334_Triplet_Subsequence.py
+++ LeetCode_75/334_Triplet_Subsequence.py
@@ -1,9 +1,16 @@
 from typing import List
 
 def increasingTriplet(nums: List[int]) -> bool:
-    max1, max2 = float('inf'), float('inf')
-    for i in nums:
-        if i > max2: return True
-        if i >= max1: max2 = i
-        else: max1 = i
+    first = float('inf')
+    second = float('inf')
+    
+    for num in nums:
+        if num <= first:
+            first = num
+        elif num <= second:
+            second = num
+        else:
+            # Found a num greater than both first and second
+            return True
+    
     return False

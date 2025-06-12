29c4057c3f241c9ad51d15eb7cf30b474c7e8bc4
zdebar <157598603+zdebar@users.noreply.github.com>
2025-06-12 18:23:55 +0200

    Prefix Sum

---
+++ LeetCode_75/04_Prefix_Sum/724_Find_Pivot_Index.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def pivotIndex(nums: List[int]) -> int:
+    right = sum(nums)
+    left = 0
+
+    for i in range(len(nums)):
+        right -= nums[i]
+        if left == right:
+            return i
+        left += nums[i]                
+    
+    return -1
